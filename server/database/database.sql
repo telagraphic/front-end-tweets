02b15e5d16bdecd7ebf46018edcb15c595b0c2ad
telagraphic <telagraphic@gmail.com>
2019-10-20 16:03:33 -0400

    data fixes for the tweeters.csv copy command

--- server/database/database.sql
+++ server/database/database.sql
@@ -27,40 +27,10 @@ create table if not exists tweeters (
 
 -- COPY tweeters(name,handle,site,topic,twitter_count,twitter_page,twitter_pic,twitter_description,twitter_followers,twitter_friends) FROM 'server/database/json/tweeters.csv' DELIMITER ',' CSV HEADER;
 
--- \copy tweeters FROM 'server/database/json/tweeters.csv' DELIMITER ',' CSV
-
--- "INSERT INTO tweets (twittertime, created, handle, message, site, topic, hashtags, link, id_string) VALUES (${twittertime}, ${created}, ${handle}, ${message}, ${site}, ${topic}, ${hashtags}, ${link}, ${id_string}) ON CONFLICT(id_string) DO NOTHING", tweet)
-
+-- \copy tweeters FROM 'server/json/tweeters.csv' DELIMITER ',' CSV HEADER
 
 -- select * from tweets;
 -- delete from tweets;
-
--- create table if not exists tweeters (
---   id serial,
---   name varchar not null,
---   handle varchar not null,
---   site varchar not null,
---   topic varchar not null,
---   twitter varchar not null
--- );
-
-
--- "created": utilities.twitterDate(tweet.created_at),
--- "handle": tweet.user.screen_name,
--- "text": tweet.text,
--- "site": tweet.user.url,
--- "topic": user.topic,
--- "hashtags": [...tweet.entities.hashtags],
--- "url": `https://www.twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
--- });
---
--- {
---   "name": "Dan Abramov",
---   "handle": "dan_abramov",
---   "site": "overreacted.io",
---   "topic": "javascript",
---   "twitter": "https://twitter.com/dan_abramov"
--- },
 
 -- run sql script
 -- psql -h ec2-54-235-246-201.compute-1.amazonaws.com -p 5432 -d db8ab9p9ordi23 -U hztnjjanxphpec -f server/database/database.sql sslmode=disable;
