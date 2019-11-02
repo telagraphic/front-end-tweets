b1a5ec878be232c7f109249e0e39b37974dac614
telagraphic <telagraphic@gmail.com>
2019-11-02 14:35:54 -0400

    added handlebars date helper, github button, started to refactor css code

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const hbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const database = require("./server/database/api");
 const path = require('path');
+const dayjs = require('dayjs');
 
 const PORT = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + '/public'));
+
+const hbs = exphbs.create({
+  partialsDir: ["public/views/partials"],
+  extname: ".hbs",
+  layoutsDir: path.join(__dirname, "public/views/layouts"),
+  defaultLayout: path.join(__dirname, "public/views/layouts/main"),
+  helpers: {
+    formatDate: function(date) {
+      return dayjs(date).format('DD/MM/YYYY [@]HH:mmA')
+    }
+  }
+});
+
 app.set("view engine", "hbs");
 app.set('views', path.join(__dirname, "/public/views"));
-app.engine(
-  "hbs",
-  hbs({
-    partialsDir: ["public/views/partials"],
-    extname: ".hbs",
-    layoutsDir: path.join(__dirname, "public/views/layouts"),
-    defaultLayout: path.join(__dirname, "public/views/layouts/main")
-  })
-);
+app.engine( "hbs", hbs.engine);
 
 app.listen(PORT, () => console.log(`Serving on ${PORT}`));
 
