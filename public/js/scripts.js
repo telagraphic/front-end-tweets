f3d0b422ca68ef44b3c0d396c6d911f113c3ef75
telagraphic <telagraphic@gmail.com>
2020-01-22 21:30:27 -0500

    📱 DESIGN: /tweeters hover fix

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,16 +9,16 @@ barba.init({
       once() {},
       beforeEnter({current, next, trigger}) {
 
-        const links = document.querySelectorAll('.nav__pages a');
-        const href = next.url.path;
-
-        links.forEach(link => {
-          if (link.getAttribute("href") === href) {
-            link.classList.add("selected");
-          } else {
-            link.classList.remove("selected");
-          }
-        });
+        // const links = document.querySelectorAll('.nav__pages a');
+        // const href = next.url.path;
+        //
+        // links.forEach(link => {
+        //   if (link.getAttribute("href") === href) {
+        //     link.classList.add("selected");
+        //   } else {
+        //     link.classList.remove("selected");
+        //   }
+        // });
 
         window.scrollTo({
           top: 0,
